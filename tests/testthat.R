aff6b09c8285e94cabc8a8cb20c915d76e2e3403
saschagobel <sascha.goebel@uni-konstanz.de>
2020-06-03 13:08:53 +0200

    split tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(legislatoR)
 
-test_check("legislatoR")
+test_check("legislatoR", filter = "cld_content")
+test_check("legislatoR", filter = "get_1")
+test_check("legislatoR", filter = "get_2")
+
